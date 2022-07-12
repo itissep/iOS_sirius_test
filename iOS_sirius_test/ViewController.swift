91815ec51b140ee3b4c34cf20d139ed438ed244d
Someone Else's Problem <someonenewish@yandex.ru>
2022-07-12 22:42:55 +0300

    Initial Commit

---
+++ iOS_sirius_test/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  iOS_sirius_test
+//
+//  Created by The GORDEEVS on 12.07.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
